068b58f55d8911a63e915ecd656fe1d91e46c1de
IronySoft <ironyoft.ltd@gmail.com>
2019-08-17 18:30:51 +0600

    Backend Layout Completed With Dashboard

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -1,23 +1,14 @@
-@extends('layouts.app')
+@extends('layouts.backend.backend-app')
+
+@section('title','Dashboard')
+
+@push('css')
+@endpush
 
 @section('content')
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-md-8">
-                <div class="card">
-                    <div class="card-header">Dashboard</div>
-
-                    <div class="card-body">
-                        @if (session('status'))
-                            <div class="alert alert-success" role="alert">
-                                {{ session('status') }}
-                            </div>
-                        @endif
-
-                        You are logged in <strong>{{Auth::user()->name}}</strong>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
+    <h1>Welcome TO Admin Dashboard</h1>
 @endsection
+
+
+@push('js')
+@endpush
