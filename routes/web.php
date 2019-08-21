851ab38700020509470fbe32c70b9d25a2632999
IronySoft <ironyoft.ltd@gmail.com>
2019-08-21 23:10:34 +0600

    Author Post Controller Created an Routed

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,7 @@ Auth::routes();
 
 Route::group(['as' => 'admin.', 'prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => ['auth', 'admin']],
     function () {
+
         Route::get('dashboard', 'DashboardController@index')->name('dashboard');
         Route::resource('tag', 'TagController');
         Route::resource('category', 'CategoryController');
@@ -21,6 +22,9 @@ Route::group(['as' => 'admin.', 'prefix' => 'admin', 'namespace' => 'Admin', 'mi
 
 Route::group(['as' => 'author.', 'prefix' => 'author', 'namespace' => 'Author', 'middleware' => ['auth', 'author']],
     function () {
+
         Route::get('/dashboard', 'DashboardController@index')->name('dashboard');
+        Route::resource('post', 'PostController');
+
     });
 
