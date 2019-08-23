3b5ff3944e4013ed43304b11ab6b840be3a297dc
IronySoft <ironyoft.ltd@gmail.com>
2019-08-23 20:41:57 +0600

    Pot and Catgeory Showed In Blog Home Page

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
+use App\Post;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -10,11 +12,7 @@ class HomeController extends Controller
      * Create a new controller instance.
      *
      * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
+
 
     /**
      * Show the application dashboard.
@@ -23,6 +21,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        $categories = Category::all();
+        $posts = Post::latest()->paginate(6);
+        return view('welcome',compact('categories','posts'));
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,8 @@
 <?php
 
-Route::get('/', function () {
-    return view('welcome');
-})->name('home');
-
 Auth::routes();
 
-//Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', 'HomeController@index')->name('home');
 
 //Subscriber
 Route::post('subscriber', 'SubscriberController@store')->name('subscriber.store');
